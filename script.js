5524db6721d3d8fed6e9bd4ec251e389d8d22dfd
colecrum26 <cwcrumrine@gmail.com>
2023-03-24 14:11:25 -0500

    started to do list

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// TODO
+// Eventually this will be replaced with an element.value in an Input tag
 const apiKey = "5bfff3ebda0e0482eab8cdd3efecce1a";
+// TODO
+// city/state will eventually be replaced
 const cityName = "new+orleans";
 const state = "LA";
 const countryCode = "US";
@@ -8,6 +12,12 @@ const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${sta
 async function getCityWeather(){
     let res = await fetch(geoUrl);
     let data = await res.json();
-    console.log(data);
+    let dataObj = data[0];
+    let lat = dataObj.lat;
+    let lon = dataObj.lon;
+    let city = dataObj.name;
+    let st = dataObj.state;
+    let country = dataObj.country
+    console.log(city, lat, lon, st, country);
 };
 getCityWeather();
